69c0cc1afa03d1323d3d03d1f8f360d3896bf2d5
Michael Miles <mmiles@genuineinteractive.com>
2013-01-23 09:03:49 -0500

    -add admin and install files - complete functionality

--- previous_references.js
+++ previous_references.js
@@ -1,8 +1,17 @@
-Drupal.behaviors.previousReferences = function (context){
-	$('.previous-reference-option').click(function(e){
-		e.preventDefault();
-		var target_field = $(this).attr('data-target-field');
-		var field_value	= $(this).attr('data-field-value');
-		$('#'+target_field).value = field_value;
-	});
-}
+(function ($) {
+//add a drupal behavior
+  Drupal.behaviors.previousReferences = {
+    attach: function(context, settings) {
+		//for all links that have class of .previoud-refernce-option
+      $('.previous-reference-option').click(function(e){
+			//stop the default link option
+			e.preventDefault();
+			//get the target field and the value
+			var target_field = $(this).attr('data-target-field');
+			var field_value	= $(this).attr('data-field-value');
+			//set target field to value
+			$('.'+target_field).val(field_value);
+		});
+    }
+  }
+})(jQuery);
